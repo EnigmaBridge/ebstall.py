159b293f3bf4f1d88d34ae07ab41a191c93ac9df
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-02 16:06:14 +0100

    util extended - default values

--- ebaws/util.py
+++ ebaws/util.py
@@ -748,6 +748,32 @@ def test_port_open(host='127.0.0.1', port=80, timeout=15, attempts=3, test_upper
     return False
 
 
+def defval(val, default=None):
+    """
+    Returns val if is not None, default instead
+    :param val:
+    :param default:
+    :return:
+    """
+    return val if val is not None else default
+
+
+def defvalkey(js, key, default=None, take_none=True):
+    """
+    Returns js[key] if set, otherwise default. Note js[key] can be None.
+    :param js:
+    :param key:
+    :param default:
+    :param take_none:
+    :return:
+    """
+    if key not in js:
+        return default
+    if js[key] is None and not take_none:
+        return default
+    return js[key]
+
+
 class DummyTCPHandler(socketserver.BaseRequestHandler):
     """Handler for a dummy socket server for firewall testing"""
     def handle(self):
