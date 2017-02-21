01e08e40ce46c79451dbdbb544cc9fad857678b0
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-21 10:06:08 +0100

    wrapper updating scripts added

---
+++ assets/scripts/local-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /home/ec2-user/ebstall && pip install --upgrade --find-links=. .
+/bin/cp ebstall-pki.sh /usr/sbin/ebstall-pki
+/bin/cp ebstall-privspace.sh /usr/sbin/ebstall-privspace
+
