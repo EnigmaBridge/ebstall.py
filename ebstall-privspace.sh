089b1d2159c9f16dac20e1332f10d303844a8b32
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-02 23:31:05 +0100

    wrapper scripts fixes #2

--- ebstall-privspace.sh
+++ ebstall-privspace.sh
@@ -131,13 +131,10 @@ fi
 # Invoke the python client directly
 SCRIPT=ebstall-privspace-cli
 
-SCRIPT_PATH=`which ${SCRIPT} 2>/dev/null`
-if [[ "$? " == 0 ]; then
-    $SCRIPT_PATH "$@"
-elif [ -f "/usr/local/bin/${SCRIPT}" ]; then
+if [ -f "/usr/local/bin/${SCRIPT}" ]; then
     /usr/local/bin/${SCRIPT} "$@"
 else
-    echoRed "Error: Script ${SCRIPT} not found."
+    `which ${SCRIPT} "$@"`
 fi
 
 
