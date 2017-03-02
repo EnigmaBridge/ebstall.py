14fb70fc04e1e71f33ac757f6b414c889983af9b
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-02 20:54:18 +0100

    openvpn - properly support single directive config lines

--- ebstall/openvpn.py
+++ ebstall/openvpn.py
@@ -235,6 +235,7 @@ class OpenVpn(object):
             last_cmd_idx = idx
             is_desired_value = cfg.params in values
             is_desired_value |= remove and len(values) == 0
+            is_desired_value |= not remove and util.is_empty(values) and util.is_empty(cfg.params)
             value_idx = values.index(cfg.params) if not remove and cfg.params in values else None
 
             if is_desired_value:
