833ed9169525ddbfe279b665afd2c4c979eb1560
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-25 11:26:13 +0000

    utils - version methods, tests

--- ebstall/util.py
+++ ebstall/util.py
@@ -30,6 +30,7 @@ import requests
 from audit import AuditManager
 from builtins import input
 from builtins import bytes
+from past.builtins import cmp
 
 import OpenSSL
 import socketserver
@@ -1191,3 +1192,208 @@ def determine_public_ip_eb(attempts=3, audit=None, host='hut6.enigmabridge.com')
     return None
 
 
+def get_repoquery_available_versions(out):
+    """
+    Processes yum list package output
+    :param out: 
+    :return: [(package name, version)]
+    """
+    ret = []
+    lines = out if isinstance(out, types.ListType) else out.split('\n')
+    for line in lines:
+        if ':' not in line:
+            continue
+
+        package, version = line.split(':', 1)
+        package = re.sub(r'-[0-9]+$', '', package)
+
+        ret.append((package, version))
+    return ret
+
+
+def repoquery_find_version(versions, min_version=None, max_version=None, exact_version=None):
+    """
+    Filters the repoquery output from get_repoquery_available_versions to contain only versions according 
+     to the criteria.
+     
+    TODO: refactor with key=lambda x for more universal usage 
+     
+    :param versions: 
+    :param min_version: 
+    :param max_version: 
+    :param exact_version: 
+    :return: 
+    """
+    return version_filter(versions, key=lambda x: x[1], min_version=min_version, max_version=max_version,
+                          exact_version=exact_version)
+
+
+def repoquery_pick_version(versions, pick_min=True, pick_max=False, max_comp=None):
+    """
+    Filters versions to one picked version
+    
+    TODO: refactor with key=lambda x for more universal usage
+    
+    :param versions: 
+    :param pick_min: 
+    :param pick_max: 
+    :param max_comp: 
+    :return: 
+    """
+    return version_pick(versions, key=lambda x: x[1], pick_min=pick_min, pick_max=pick_max, max_comp=max_comp)
+
+
+def version_filter(objects, key=lambda x: x, min_version=None, max_version=None, exact_version=None):
+    """
+    Filters the objects according to the version criteria.
+    :param objects: 
+    :param key: 
+    :param min_version: 
+    :param max_version: 
+    :param exact_version: 
+    :return: array of objects matching all criteria given
+    """
+    ret = list(objects)
+
+    if exact_version is not None:
+        ret = [x for x in ret if version_cmp(key(x), exact_version, max_comp=version_len(exact_version)) == 0]
+
+    if min_version is not None:
+        ret = [x for x in ret if version_cmp(key(x), min_version, max_comp=version_len(min_version)) >= 0]
+
+    if max_version is not None:
+        ret = [x for x in ret if version_cmp(key(x), max_version, max_comp=version_len(max_version)) <= 0]
+
+    return ret
+
+
+def version_pick(objects, key=lambda x: x, pick_min=False, pick_max=False, max_comp=None):
+    """
+    Filters objects according to the version criteria.
+    Picks either minimal version from the list or the maximal one
+    :param objects: 
+    :param key: 
+    :param pick_min: 
+    :param pick_max: 
+    :param max_comp: 
+    :return: array of objects with the same picked version
+    """
+    all_versions = set()
+    for ver in objects:
+        v = version_trim(key(ver), max_comp)
+        all_versions.add(v)
+
+    all_versions = list(all_versions)
+    all_versions = sorted(all_versions, cmp=version_cmp)
+    selected = None
+
+    if pick_min:
+        selected = all_versions[0]
+    if pick_max:
+        selected = all_versions[-1]
+
+    return [x for x in objects if version_cmp(key(x), selected, max_comp=max_comp) == 0]
+
+
+def version_len(a, version_delim='.'):
+    """
+    Returns number of version components
+    :param a: 
+    :param version_delim: 
+    :return: 
+    """
+    if isinstance(a, types.ListType):
+        return len(a)
+
+    return len(a.split(version_delim))
+
+
+def version_trim(a, max_comp=None):
+    """
+    Trims the version to the given length
+    :param a: 
+    :param max_comp: 
+    :return: 
+    """
+    if max_comp is None:
+        return a
+
+    if isinstance(a, types.ListType):
+        return a[:max_comp]
+
+    p = a.split('.')
+    return '.'.join(p[:max_comp])
+
+
+def version_pad(a, ln):
+    """
+    Pads version with zeros to the given component length
+    :param a: 
+    :param ln: 
+    :return: 
+    """
+    vlen = version_len(a)
+    if vlen >= ln:
+        return a
+
+    if isinstance(a, types.ListType):
+        return a + ([0] * (ln - vlen))
+
+    p = a.split('.')
+    return '.'.join(p + (['0'] * (ln - vlen)))
+
+
+def version_cmp(a, b, max_comp=None, version_delim='.'):
+    """
+    Compares versions a, b lexicographically
+    :param a: 
+    :param b: 
+    :param max_comp: maximal number of descent
+    :param version_delim: version delimitier
+    :return: 
+    """
+    def v_split(x, delim):
+        if isinstance(x, types.IntType):
+            return [x]
+        if isinstance(x, types.ListType):
+            return x
+        return x.split(delim)
+
+    parts_a = v_split(a, version_delim)
+    parts_b = v_split(b, version_delim)
+    cmp_len = max(len(parts_a), len(parts_b))
+
+    # Pad with zeros so 5.3 < 5.3.4
+    parts_a = version_pad(parts_a, cmp_len)
+    parts_b = version_pad(parts_b, cmp_len)
+
+    if max_comp is not None:
+        cmp_len = min(cmp_len, max_comp)
+
+    for idx in range(cmp_len):
+        if version_delim == '.':
+            cmp_res = version_cmp(parts_a[idx], parts_b[idx], max_comp=None, version_delim='-')
+        else:
+            cmp_res = cmp(int_if_int(parts_a[idx]), int_if_int(parts_b[idx]))
+
+        if cmp_res == 0:
+            continue
+        return cmp_res
+
+    # Tie
+    return 0
+
+
+def int_if_int(x):
+    """
+    Converts to integer if it is integral
+    :param x: 
+    :return: 
+    """
+    if isinstance(x, types.IntType):
+        return x
+
+    if re.match('^[0-9]', x):
+        return int(x)
+
+    return x
