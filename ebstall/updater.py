e98da88253c4a4a5078c38ab7ee1d217f0ba3c80
Dusan Klinec <dusan.klinec@gmail.com>
2017-04-18 17:03:22 +0200

    updater - rule eval, yaql_cli method
    - rule eval will be used in the update rules

--- ebstall/updater.py
+++ ebstall/updater.py
@@ -21,6 +21,8 @@ import re
 import errors
 import consts
 import json
+from functools import reduce
+import types
 import collections
 from audit import AuditManager
 
@@ -43,12 +45,12 @@ class Updater(object):
     """
     Updating the private space
     """
-    def __init__(self, config=None, audit=None, syscfg=None):
+    def __init__(self, config=None, audit=None, sysconfig=None):
         self.engine = None
         self.root = None
         self.config = config
         self.audit = audit
-        self.syscfg = syscfg
+        self.syscfg = sysconfig
 
     def init_parser(self):
         """
@@ -79,14 +81,14 @@ class Updater(object):
         versions_yaql.register(ctxt, self.engine)
         return ctxt
 
-    def gen_data(self):
+    def gen_rule_data(self):
         """
         Generates the root data object for the YAQL evaluation.
         :return: 
         """
         self.root = collections.OrderedDict()
 
-        # Config & versions
+        # Config & versions, abbrevs
         self.root['config'] = self.config
 
         self.root['ebstall_version_initial'] = versions.Version(self.config.ebstall_version_initial) \
@@ -97,6 +99,9 @@ class Updater(object):
 
         self.root['install_version'] = versions.Version(self.config.install_version) \
             if self.config is not None else versions.Version('0')
+
+        self.root['source_image_code'] = self.config.source_image_code \
+            if self.config is not None else None
 
         # Current OS info
         self.root['os'] = self.syscfg.get_os()
@@ -114,9 +119,9 @@ class Updater(object):
 
         return self.root
 
-    def eval(self, expr, ctx=None):
+    def eval_single(self, expr, ctx=None):
         """
-        Evaluates an expression
+        Evaluates a single expression
         :param expr: 
         :return: 
         """
@@ -129,20 +134,71 @@ class Updater(object):
         res = self.engine(expr).evaluate(self.root, ctx)
         return res
 
+    def eval(self, expr, ctx=None, lazy=False):
+        """
+        Evaluates a single expression / list of expressions
+        :param expr: 
+        :param ctx: 
+        :param lazy: evaluates from the first element, stops if some expression evaluates to None / False
+        :return: 
+        """
+        if isinstance(expr, types.ListType):
+            if not lazy:
+                return [self.eval_single(x, ctx) for x in expr]
+
+            # Lazy eval
+            res = []
+            finish_lazy_none = False
+            for x in expr:
+                if finish_lazy_none:
+                    res.append(None)
+                    continue
+
+                cur_res = self.eval_single(expr, ctx)
+                res.append(cur_res)
+
+                if cur_res is None or not cur_res:
+                    finish_lazy_none = True
+
+        else:
+            return self.eval_single(expr, ctx)
+
+    def eval_rule(self, rule, ctx=None):
+        """
+        Evals rule from the update definitions
+        Returns a single value
+        
+        :param rule: 
+        :param ctx: 
+        :return: 
+        """
+        res = self.eval(rule, ctx, lazy=True)
+        if isinstance(rule, types.ListType):
+            return reduce(lambda x, y: x and y, res)
+        else:
+            return res
+
+    def yaql_cli(self):
+        """
+        Spawns yaql interactive interface
+        (Used for debugging)
+        :return: 
+        """
+        self.init_parser()
+        self.gen_rule_data()
+
+        # CLI hack
+        ctx = self.new_context()
+        cli_functions.register_in_context(ctx, self.engine)
+        self.eval('__main(false)', ctx)
+
 
 if __name__ == "__main__":
-    data = {}
-    engine_options = {
-        'yaql.limitIterators': 1000,
-        'yaql.convertSetsToLists': True,
-        'yaql.memoryQuota': 100000
-    }
-
     config = None
     syscfg = SysConfig()
     updater = Updater(syscfg=syscfg, config=config)
     updater.init_parser()
-    updater.gen_data()
+    updater.gen_rule_data()
 
     # CLI hack
     ctx = updater.new_context()
